43e589791c94469d117c03891ba97905261f6631
captain2376 <b17296@rgukt.ac.in>
2023-07-06 11:03:10 +0530

    c2

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -14,6 +14,9 @@ import React, { Component } from 'react'
         axios.get('https://jsonplaceholder.typicode.com/posts')
         .then(response=>{
             console.log(response);
+            this.setState({
+                posts:response.data
+            })
         })
         .catch(error=>{
             console.log(error);
@@ -36,3 +39,13 @@ import React, { Component } from 'react'
 }
 
 export default PostList
+
+//flow starts with constructor state property called posts
+//which is an empty array
+//control flows to render method :text list of posts is displayed
+//actual list is not rendered because the array is empty at the moment
+//control flows to componentDidMount() we make get requeset to api endpoint
+//once data is retrieved update the state post property
+//when we change state,component will re-render
+//this time array is not empty ande hence the list of titles are
+//rendered in the browser
